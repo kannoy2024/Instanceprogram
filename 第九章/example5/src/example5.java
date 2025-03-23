439cebe492e9ead44932de7f3384c4d6496aa716
kannoy <zuzhenhuang365@gmail.com>
2025-03-23 20:23:12 +0800

    Implement box layout

--- 第九章/example5/src/example5.java
+++ 第九章/example5/src/example5.java
@@ -2,6 +2,6 @@ public class example5 {
 public static void main(String[] args) {
     WindowBoxLayout wbl = new WindowBoxLayout();
     wbl.setBounds(100, 100, 200, 200);
-    wbl.setTitle("Nested layout container");
+    wbl.setTitle("Nested box layout ");
 }
 }
