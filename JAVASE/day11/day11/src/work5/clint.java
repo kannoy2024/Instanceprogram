6e17064dc160831fb707c34eaf47515bdaa2b366
kannoy <zuzhenhuang365@gmail.com>
2025-08-15 22:12:05 +0800

    优化了文件传输的内容。使用循环来保证服务器一直接受数据

--- JAVASE/day11/day11/src/work5/clint.java
+++ JAVASE/day11/day11/src/work5/clint.java
@@ -1,36 +1,30 @@
 package work5;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
 
-public class clint {
+public class Clint {
     public static void main(String[] args) {
-
         try (Socket socket = new Socket("127.0.0.1", 9999);
-                OutputStream os = socket.getOutputStream();
-                FileInputStream fis = new FileInputStream(new File("F:\\MaterialLibrary\\apple.jpg"));
-                InputStream is = socket.getInputStream();
-                InputStreamReader isr = new InputStreamReader(is);
-                BufferedReader br = new BufferedReader(isr);) {
+             OutputStream os = socket.getOutputStream();
+             FileInputStream fis = new FileInputStream(new File("F:\\MaterialLibrary\\apple.jpg"));
+             InputStream is = socket.getInputStream();
+             InputStreamReader isr = new InputStreamReader(is);
+             BufferedReader br = new BufferedReader(isr)) {
 
             System.out.println("开始发送数据");
-            os.write("你好".getBytes());
-
+            
             byte[] bytes = new byte[1024];
             int len = -1;
             while ((len = fis.read(bytes)) != -1) {
                 os.write(bytes, 0, len);
             }
-            socket.shutdownOutput();
+            socket.shutdownOutput(); // 关闭输出流，表示发送完毕
+            
             System.out.println("发送完毕");
 
             String line = br.readLine();
-            System.out.println("客户端接收到的数据"+line);
+            System.out.println("客户端接收到的数据: " + line);
         } catch (Exception e) {
             e.printStackTrace();
         }
